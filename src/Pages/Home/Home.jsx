12466687a5fa63ae3cb212ce00281c8edef6a3a7
akella v s sreenivasu <128373974+SreenivasuAkella@users.noreply.github.com>
2023-09-07 00:10:59 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,12 +6,14 @@ function Home() {
         <section id="hero-section-1" className="section-main">
           <div className="hero-content-wrapper">
             <div className="hero-main">
-              <h1>Lorem ipsum dolor sit amet</h1>
+              <h1>Parsec4.0</h1>
               <div className={"hero-quote-wrapper"}>
                 <div className={"style-quote-bar"}></div>
                 <div className={"hero-quote"}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Witness the collision of genius minds and cutting-edge technology at IIT Dharwad's most electrifying Technical
+                 Fest- Parsec 4.0, a rendevous with the future! Get ready to dive into the sea of circuits & algorithms, machines 
+                 & robots, bytes and bits, zeroes & ones. Let your imagination and innovation take flight in this celebration of 
+                 brilliance and creativity.
                 </div>
               </div>
             </div>
@@ -22,12 +24,14 @@ function Home() {
           <div className="section-main">
             <div className="hero-content-wrapper">
               <div className="hero-main">
-                <h1>Lorem ipsum dolor sit amet</h1>
+                <h1>ABOUT US</h1>
                 <div className={"hero-quote-wrapper"}>
                   <div className={"hero-quote"}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                  Parsec 2024 is the fourth edition of the Science and Technology festival of IIT Dharwad.Parsec is an unparalleled opportunity for budding technologists to showcase their technical prowess and acts as the perfect launchpad for brilliant ideas.<br></br>
+
+Parsec boasts a wide array of events, competitions, workshops and lectures designed to rekindle the passion for technology and business among the brightest minds of the nation. The fest will also host lectures by experts from a multitude of fields.<br></br>
+
+In addition to the plethora of technical events, Parsec promises to be a fun enthused extravaganza with numerous cultural events for the participants to enjoy. We welcome everyone to be a part of this technocultural festival!!
                   </div>
                 </div>
               </div>
@@ -36,10 +40,10 @@ function Home() {
           <div id="home-animate-2" className="buffer-space"></div>
         </section>
 
-        <section id="hero-section-3" className="section-main">
+       {/*} <section id="hero-section-3" className="section-main">
           <div className="hero-content-wrapper">
             <div className="hero-main">
-              <h1>Lorem ipsum dolor sit amet</h1>
+              <h1>Sponsors</h1>
               <div className={"hero-quote-wrapper"}>
                 <div className={"hero-quote"}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -74,9 +78,9 @@ function Home() {
               </div>
             </div>
           </div>
-        </section>
+        </section>*/}
       </div>
-    </>
+  </>
   )
 }
 
