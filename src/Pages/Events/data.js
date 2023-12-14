6c2840c63748533537fee5f3ee78270b2cae1030
Utkarsh1343 <ahmednagaruser414006@gmail.com>
2023-12-14 23:15:51 +0530

    Event Photos updated

--- src/Pages/Events/data.js
+++ src/Pages/Events/data.js
@@ -3,68 +3,77 @@ const data = [
     heading: "DevHack 5.0",
     content:
       "DevHack 5.0, the next edition of the annual hackathon of IIT Dharwad is back - bigger, better and bolder! We have always believed that engineers and researchers have the power to revolutionize the world by transforming their inspiring ideas into solutions that positively impact society. Prove your worth in this 36-hour hackathon by ideating, innovating and hacking meaningful solutions to real life problems.",
-    image: "/Images/Eventimage.png",
+    image: "/Images/DevHack.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/15gb-OIBMqm8VlWeJ3AOWYgFbwCTwLM4d/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "PC IIT Dharwad",
+    timing: "2 Feb, 2:00 PM",
   },
   {
     heading: "AlgoStrike 4.0",
     content: `An algorithmic programming contest which tests your knowledge of various heuristic algorithms. The objective of the contest is that the participants have to formulate solutions to a multitude of fascinating engineering problems utilizing heuristic algorithms. Get set algo!`,
-    image: "/Images/mascot.png",
+    image: "/Images/AlgoStrike.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/1gYahKSXWV9wx5_pXfKa22FHRrT18kFV4/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "PC IIT Dharwad",
+    timing: "3 Feb, 10:30 AM - 12:30 PM",
   },
   {
-    heading: "Stonkz 3.0",
-    content: `PARSEC 4.0, the annual technological symposium hosted by IIT Dharwad, presents Investor Summit the Mock Trading Competition—an intellectually challenging online mock-stock event meticulously designed to assess participants' investment acumen in the volatile financial markets. `,
-    image: "/Images/mascot.png",
+    heading: "BitHunt 2.0",
+    content: `Gather your squad of four and dive into BitHunt 2.0! This campus-wide event challenges your problem-solving skills, data structures, and algorithms in two thrilling rounds. Round 1, a quiz selection, will test your team's knowledge, paving the way for the top 8 contenders to engage in a color-coded treasure hunt in Round 2. Who will decipher the clues, conquer the campus, and claim the coveted title? Join BitHunt 2.0 for an unforgettable blend of intellect and adventure! `,
+    image: "/Images/BitHunt.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/1UpLDai46hG_Xhu4E1YjxeHKwe82lXAaV/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "Online",
+    timing: "20 Jan, 10:00 AM",
+  },
+  {
+    heading: "Tetris Trades",
+    content: `PARSEC 4.0, the annual technological symposium hosted by IIT Dharwad, presents Investor Summit the Mock Trading Competition—an intellectually challenging online mock-stock event meticulously designed to assess participants' investment acumen in the volatile financial markets. `,
+    image: "/Images/TetrisTrades.jpg",
+    registrationLink:
+      "",
+    location: "Online",
+    timing: "26 Jan - 2 Feb",
   },
   {
     heading: "AI Cure: Where AI Meets Healing Touch",
     content: `AI is transforming disease detection by swiftly analyzing extensive datasets, enabling early identification, and enhancing healthcare outcomes. Machine learning algorithms, when trained
       on comprehensive datasets, can discern subtle patterns and correlations that may elude human practitioners. The objective of this hackathon is to construct a machine-learning model to address such challenges.
       `,
-    image: "/Events/AIwars.jpg",
+    image: "/Images/AICure.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/1I-VS6n45cYAmGtECH3lCGQpSUYQzBIU-/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "Online",
+    timing: "2 Feb, 8:30 PM - 10:30PM",
   },
   {
     heading: "Aurora: A Stellar Odyssey",
     content: `Ahoy, fellow spacefarers! Ascend with us on a cosmic quest to unravel the mysteries plaguing the Deep Space Climate Observatory (DSCOVR)! Our mission? To crack the code behind its perplexing anomalies by crafting two separate models for predicting geomagnetic storms. But there's a twist – we'll be tackling this challenge using both raw and meticulously calibrated data sets, pushing the boundaries of our knowledge and ingenuity. We're even throwing in a bonus challenge for the truly audacious: devise innovative methods to transform raw data into valuable insights, unlocking secrets hidden within its depths. So, buckle up, sharpen your pencils, and prepare for an astronomical adventure in the realm of space weather forecasting!
       `,
-    image: "/Images/Eventimage.png",
+    image: "/Images/Aurora.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/1Y2tmllwBCiio3Tqv4EV_jyR7T0Q2UDQM/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "PC IIT Dharwad",
+    timing: "2 Feb, 5:00 PM - 8:00PM",
   },
   {
     heading: "Build-a-bot",
     content: `Join us at IIT Dharwad for Build-a-bot, an electrifying robot buildathon during PARSEC 4.0, the annual techfest. Teams of 3-4 members will immerse themselves in a 36-hour challenge, designing, building, and programming a robot to tackle a surprise task revealed on the spot. The competition evaluates robots based on functionality, design, and innovation`,
-    image: "/Events/Route_AI.jpg",
+    image: "/Images/Build-a-bot.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/14nf6hInkkpsOvk6v_oCVhfxwwvXXr1tw/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "PC IIT Dharwad",
+    timing: "2 Feb, 7:00 PM",
   },
   {
     heading: "FinTech Odyssey",
     content: `Dive into the FinTech Odyssey, a dynamic event blending knowledge and adventure. In the TechFin Trivia Trek, teams of three navigate a 15-minute quiz, showcasing their FinTech acumen. Top 5 teams progress to the immersive TechFin Challenge Quest, delving into the intricacies of a FinTech company. Are you ready to chart the course for the future of finance? Join the Odyssey! `,
-    image: "/Events/Rovernomics.jpg",
+    image: "/Images/Fintech.jpg",
     registrationLink:
-      "https://drive.google.com/file/d/1v5P_gvIA_YXFxLCZSGHlEbBF5xB9TGdI/view?usp=sharing",
-    location: "Central Lecture theatre",
-    timing: "29th Feb, 3:00 PM - 5:00PM",
+      "",
+    location: "Online",
+    timing: "20 Jan",
   },
 ]
 
