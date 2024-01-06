56e412ab6a88b7d1016df9bfe2bf39466a975fab
K12TK013A1N <shourya15singh7@gmail.com>
2024-01-06 16:39:18 +0530

    Changes in team page (data updated).

--- src/Pages/Team/data.js
+++ src/Pages/Team/data.js
@@ -26,6 +26,30 @@ const teamsData = [
       },
     ],
   },
+
+  {
+    name: "EVENTS HEAD",
+    members: [
+      {
+        name: "GURVIR SINGH",
+        image: "https://drive.google.com/uc?id=1fo9h0ogSZeSGenwLAYbfGHw4sEVAdGSf",
+        email: "210020012@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/gurvirbhatti?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+        twitter: "",
+        instagram: "https://www.instagram.com/gurvirus?igsh=MTM4cjl3cWlvOGNuMQ%3D%3D&utm_source=qr",
+      },
+      // {
+      //   name: "XYZ",
+      //   image: "/Team/xyz.jpg",
+      //   email: "",
+      //   linkedin: "",
+      //   twitter: "",
+      //   instagram: "",
+      // },
+    ]
+  },
+
+
   {
     name: "WEB-TEAM",
     members: [
@@ -40,134 +64,29 @@ const teamsData = [
         instagram: "https://www.instagram.com/vedan.obs/",
       },
       {
-        name: "XYZ",
-        image: "/Team/xyz.jpg",
-        email: "",
-        linkedin: "https://in.linkedin.com/in/narendramodi",
+        name: "SHASHWAT SHOURYA",
+        image: "https://drive.google.com/uc?id=14OuerpMEPizBT5Vldec2cDtqO2LJ-3b6",
+        email: "220110008@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/shashwat-shourya-3b0812226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         twitter: "https://twitter.com/elonmusk/",
-        instagram: "",
+        instagram: "https://www.instagram.com/shashwat_shourya_?igsh=MWd6MzJ6b2V4b3diZw==",
       },
       {
-        name: "XYZ",
-        image: "/Team/xyz.jpg",
-        email: "",
-        linkedin: "",
+        name: "UTKARSH GUPTA",
+        image: "https://drive.google.com/uc?id=1xa0ggya4rI--8bNQFMAkzXkJ-gTZKR6d",
+        email: "220020056@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/utkarsh-gupta-812308270/",
         twitter: "https://youtu.be/dQw4w9WgXcQ",
-        instagram: "https://www.instagram.com/virat.kohli/",
+        instagram: "https://www.instagram.com/utkarshgupta_7700/",
       },
-    ],
-  },
-
-  // {
-  //   name: "SPACE DATA SCIENCE TEAM",
-  //   members: [
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "https://youtu.be/dQw4w9WgXcQ",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //   ],
-  // },
-
-  // {
-  //   name: "ARTIFICIAL INTELLIGENCE TEAM",
-  //   members: [
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //   ],
-  // },
-
-  // {
-  //   name: "ROBOTICS TEAM",
-  //   members: [
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //     {
-  //       name: "XYZ",
-  //       image: "/Team/xyz.jpg",
-  //       email: "",
-  //       linkedin: "",
-  //       twitter: "",
-  //       instagram: "",
-  //     },
-  //   ],
-  // },
-
-  {
-    name: "HEAD OF FINANCE",
-    members: [
       {
-        name: "SHUBH AGARWAL",
-        image:
-          "https://drive.google.com/uc?id=1VjDo8BhiRBHX4ckyCRhd8QV4zepz3RpZ",
-        email: "210020047@iitdh.ac.in",
-        linkedin: "https://www.linkedin.com/in/shubhagarwal3110",
-        twitter: "",
-        instagram: "https://www.instagram.com/_shubhag_",
+        name: "AKELLA SREENIVASU",
+        image: "https://drive.google.com/uc?id=1P-a5t-VlTIByEUjTugjQCUjoY3MkPD_P",
+        email: "210020001@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/akella-v-s-sreenivasu-639686269",
+        twitter: "https://youtu.be/dQw4w9WgXcQ",
+        instagram: "https://www.instagram.com/akella_sreenivasu?igsh=MjRiZG1jNzdrMGJ6",
       },
-      // {
-      //   name: "XYZ",
-      //   image: "/Team/xyz.jpg",
-      //   email: "",
-      //   linkedin: "",
-      //   twitter: "",
-      //   instagram: "",
-      // },
     ],
   },
 
@@ -184,30 +103,54 @@ const teamsData = [
         twitter: "",
         instagram: "https://www.instagram.com/aiden_tempest",
       },
-      // {
-      //   name: "XYZ",
-      //   image: "/Team/xyz.jpg",
-      //   email: "",
-      //   linkedin: "",
-      //   twitter: "",
-      //   instagram: "",
-      // },
-      // {
-      //   name: "XYZ",
-      //   image: "/Team/xyz.jpg",
-      //   email: "",
-      //   linkedin: "",
-      //   twitter: "",
-      //   instagram: "",
-      // },
-      // {
-      //   name: "XYZ",
-      //   image: "/Team/xyz.jpg",
-      //   email: "",
-      //   linkedin: "",
-      //   twitter: "",
-      //   instagram: "",
-      // },
+      {
+        name: "ATHARVA TIJARE",
+        image: "https://drive.google.com/uc?id=1fEb_s4FkDzoMVR7STpxpDWYpoaWoRPDn",
+        email: "220010007@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/atharva-virendra-tijare-b0a926258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "",
+        instagram: "https://www.instagram.com/atharva.tijare?igsh=ODA1NTc5OTg5Nw==",
+      },
+      {
+        name: "UPAKRAMANIKA BISHNU",
+        image: "https://drive.google.com/uc?id=1gJ3mqjgKFSdAjskcL58vpt2je0Z9evpa",
+        email: "225100039@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/upakramanika-bishnu-339901254/",
+        twitter: "",
+        instagram: "https://www.instagram.com/thequestionmark?igsh=MTM0dWs2d2hqNW9xbg==",
+      },
+      {
+        name: "AAYUSH VATS",
+        image: "https://drive.google.com/uc?id=1Uk7zNFnlJES3gJphS6dOlxqt3_rCrtZN",
+        email: "220010001@iitdh.ac.in",
+        linkedin: "http://www.linkedin.com/in/aayush-vats-451b6b269",
+        twitter: "",
+        instagram: "https://www.instagram.com/astrogeek.iitdh?igsh=MTdsbHY1OG11NWR6Zw==",
+      },
+      {
+        name: "GAURAV SINGHAL",
+        image: "https://drive.google.com/uc?id=1_wwsMt-Mf5GrconbLTwwqHt4MwHiyiAs",
+        email: "220090014@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/gaurav-singhal-5b0805265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "",
+        instagram: "https://www.instagram.com/gauravsinghal111?igsh=MXh3dGNlcnZ6ZmpwZg==",
+      },
+      {
+        name: "AYUSH SINGHI",
+        image: "https://drive.google.com/uc?id=1DllCfwI5MfEuRg6ObwHiE9o8p4lTl9Kh",
+        email: "210020048@iitdh.ac.in",
+        linkedin: "",
+        twitter: "",
+        instagram: "https://www.instagram.com/ayush_singhi_12?igsh=MTRqbG4xNDVwemowNQ==",
+      },
+      {
+        name: "ANJALI KATAGERI",
+        image: "https://drive.google.com/uc?id=1o1p3T-W-rY8hO8D0oR5fo2_sv3nKQCgT",
+        email: "225100018@iitdh.ac.in",
+        linkedin: "",
+        twitter: "",
+        instagram: "https://www.instagram.com/ayush_singhi_12?igsh=MTRqbG4xNDVwemowNQ==",
+      },
       // {
       //   name: "XYZ",
       //   image: "/Team/xyz.jpg",
@@ -246,8 +189,118 @@ const teamsData = [
         twitter: "",
         instagram: "https://www.instagram.com/om. .sd",
       },
+      {
+        name: "SAI DHRUTHI K V L ",
+        image: "https://drive.google.com/uc?id=1AkZAo9COxgwyhQVMTs8vEqD7i5HvRVcJ",
+        email: "220090012@iitdh.ac.in",
+        linkedin: "http://www.linkedin.com/in/sai-dhruthi-k-v-l-890337260",
+        twitter: "",
+        instagram: "https://www.instagram.com/dhruthii6561?igsh=NzRqeGZlc3RnNm1m",
+      },
+      {
+          name: "PRANAV KUMAR PANDEY",
+          image: "https://drive.google.com/uc?id=1yaWXyHC-C_wCnVbbBjdezjeDTqjnrdDr",
+          email: "220010043@iitdh.ac.in",
+          linkedin: "https://www.linkedin.com/in/pranav-kumar-pandey-001388250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+          twitter: "",
+          instagram: "https://www.instagram.com/pranavpandeyghq123?igsh=cTF1eXRwajRvdzhw",
+      },
     ],
   },
+
+  {
+    name: "ARTIFICIAL INTELLIGENCE TEAM",
+    members: [
+      {
+        name: "SRIHARI K G",
+        image: "https://drive.google.com/uc?id=1g3zcSs6FC9t7VIApLjACAapgrG4KAnnI",
+        email: "210030035@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/srihari-k-g-91931a1ab/",
+        twitter: "",
+        instagram: "https://www.instagram.com/srihari_k_g_2003/",
+      },
+      // {
+      //   name: "XYZ",
+      //   image: "/Team/xyz.jpg",
+      //   email: "",
+      //   linkedin: "",
+      //   twitter: "",
+      //   instagram: "",
+      // },
+    ],
+  },
+
+  {
+    name: "ALGOSTRIKE TEAM",
+    members: [
+      {
+        name: "TELLA RAJASHEKHAR REDDY ",
+        image: "https://drive.google.com/uc?id=1r9jpCKsoUvsM5dWH78HoEJZM4g8wQs4H",
+        email: "200030058@iitdh.ac.in",
+        linkedin: "",
+        twitter: "",
+        instagram: "https://www.instagram.com/rajshekhar_091?igsh=dndlMzNiNjM2eXgz",
+      },
+    ]
+  },
+
+  {
+    name: "BITHUNT 2.0 TEAM",
+    members: [
+      {
+        name: "PAVAN KUMAR V PATIL",
+        image: "https://drive.google.com/uc?id=12R1oPxj37I2ZzeQp6B2dno_QdrOIr-mc",
+        email: "200030041@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/pavan-kumar-v-patil-8693a0200/",
+        twitter: "",
+        instagram: "",
+      },
+    ]
+  },
+
+  {
+    name: "BUILD-A-THON TEAM",
+    members: [
+      {
+        name: "ANIMESH JAIN ",
+        image: "https://drive.google.com/uc?id=1HWWC_cnTtQO_TdbFqNTtA6umZcmh8Wr5",
+        email: "220120005@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/animesh-jain-741423192/",
+        twitter: "",
+        instagram: "https://www.instagram.com/animeshjain221096?igsh=MXJ6amVqMXBxc2U0OA=="
+      },
+    ]
+  },
+
+  {
+    name: "FINTECH ODYSSEY TEAM",
+    members: [
+      {
+        name: "JAI SHARMA",
+        image: "https://drive.google.com/uc?id=1MmkJJK4cOxT_eEKJAhrG2j8wCO2LDtxE",
+        email: "220120009@iitdh.ac.in",
+        linkedin: "www.linkedin.com/in/thelifeofjai",
+        twitter: "",
+        instagram: "https://www.instagram.com/jaisharma___?igsh=MWg3c3ZzYXp1eXk5MA==",
+      }
+    ]
+  },
+
+  {
+    name: "AURORA- A STELLAR ODYSSEY TEAM LEAD",
+    members: [
+      {
+        name: "ABHIRAM K",
+        image: "https://drive.google.com/uc?id=1pSFviJE2O17oc3PHWVT5CuOCgLrta3mz",
+        email: "210150001@iitdh.ac.in",
+        linkedin: "https://www.linkedin.com/in/abhiram-k-837a89223?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "",
+        instagram: "",
+      },
+    ],
+  },
+
+
 ]
 
 export default teamsData
