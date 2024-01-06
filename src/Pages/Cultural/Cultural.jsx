bcbb1bd2a3b91c085a327d363ae67248cfb1657f
Utkarsh1343 <ahmednagaruser414006@gmail.com>
2024-01-06 16:52:20 +0530

    Basic Cultural Page setup

--- src/Pages/Cultural/Cultural.jsx
+++ src/Pages/Cultural/Cultural.jsx
@@ -1,5 +1,49 @@
+import React from "react";
+import './Cultural.css';
+import photo from '/Images/DJ.jpg';
 function Cultural() {
-  return <div className="pt-20 relative">Cultural Page</div>
+  return (
+  <div className="pt-20 relative">
+    <main>
+  <div className= "card">
+    <img src={photo} alt="DJ image"/>
+    <div className="card-content font-hero font-bold">
+      <h2>
+        Binary Beats
+      </h2>
+      <p>
+        DJ Night organised by Parsec 4.0.
+      </p>
+      <a href="#" className="button">
+        Pay Here
+        <span className="material-symbols-outlined">
+          arrow_right_alt
+        </span>
+      </a>
+    </div>
+  </div>
+</main>
+<main>
+  <div className = "card">
+    <img src={photo} alt="DJ image"/>
+    <div className="card-content font-hero font-bold">
+      <h2>
+        Binary Beats
+      </h2>
+      <p>
+        DJ Night organised by Parsec 4.0.
+      </p>
+      <a href="#" className="button">
+        Pay Here
+        <span className="material-symbols-outlined">
+          arrow_right_alt
+        </span>
+      </a>
+    </div>
+  </div>
+</main>
+  </div>
+  );
 }
 
 export default Cultural
